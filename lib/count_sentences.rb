dac3c3077af480d8421316db69969199683209b6
William Luck <luck.william.h@gmail.com>
2022-08-08 11:28:28 -0500

    commit tree 3888d0431859dc2a3df9878482ebca73b7cdd1f0

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    # Using regular expression for split. From my understanding, the split will look for the characters within the forward slashes. The characters within the brackets are the specific characters to look for. I cannot omit the brackets because the ? and ! will correspond to other shorthand symbols I think 
+    (self.split(/[!.?]/) - ['', nil]).count
   end
 end
+
+binding.pry
+0
